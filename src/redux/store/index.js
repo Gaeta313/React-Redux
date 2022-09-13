fd4dceaf431d24a837447733a9d1a44c2e60a79c
Gaetano Giulio Notarangelo <102958789+Gaeta313@users.noreply.github.com>
2022-09-13 16:20:30 +0200

    aggiornato

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,18 +1,38 @@
 // questo file si occupa di creare lo store all'avvio dell'applicazione
 
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import mainReducer from '../reducers' // basta puntare la cartella
-import favouritesReducers from '../reducers/favouritesReducers'
-import searchReducers from '../reducers/searchReducer'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import mainReducer from "../reducers"; // basta puntare la cartella
+import favouritesReducers from "../reducers/favouritesReducers";
+import searchReducers from "../reducers/searchReducer";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import { persistStore, persistReducer } from "redux-persist";
 // dove è contenuto il file index.js
+
+const persistConfig = {
+  key: "root", // il livello da cui vogliamo cominciare a far persistere i dati
+  storage, // come dire --> storage: storage, seleziona lo storage engine da utilizzare
+  transforms: [
+    // elenco delle trasformazioni (plugin) applicabili a redux-persist
+    encryptTransform({
+      secretKey: "chiave",
+    }),
+  ],
+};
 
 const bigReducers = combineReducers({
   favourites: favouritesReducers,
-  search: searchReducers
-})
+  search: searchReducers,
+});
 
-const store = configureStore({
-  reducer: bigReducers,
-})
+const persistedReducer = persistReducer(persistConfig, bigReducers);
 
-export default store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // <-- lo spegne
+    }),
+});
+
+export const persistor = persistStore(store);
